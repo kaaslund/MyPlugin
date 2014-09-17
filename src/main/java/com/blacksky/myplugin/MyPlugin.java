5f15c15a2e8b4989e39a04190bfe32548d6c7e23
Kristian Aaslund <kristian.aaslund@gmail.com>
2014-09-17 19:50:21 +0200

    working plugin

--- src/main/java/com/blacksky/myplugin/MyPlugin.java
+++ src/main/java/com/blacksky/myplugin/MyPlugin.java
@@ -1,5 +1,35 @@
 package com.blacksky.myplugin;
 
-public class MyPlugin {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyPlugin extends JavaPlugin {
     
+    private static MyPlugin instance;
+    
+    public static MyPlugin getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        getLogger().info("MyPlugin Started!");
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("hello")) {
+            sender.sendMessage("world");
+        }
+
+        return false;
+    }
+
 }
